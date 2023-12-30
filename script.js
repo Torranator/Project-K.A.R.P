b0f5e3214c15b64cf26a1cc34777b43c091ce7c1
Torranator <filatovyaroslave@gmail.com>
2023-12-31 01:40:00 +0300

    kinda made it in array
    the only issue is that doesnt wanna add elements in one

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-// K.A.R.P v0.1 unstable
-function addCard() {
-  const crossName = document.querySelector("#crossName");
-  const crossDescription = document.querySelector("#crossDescription");
-  const crossImage = document.querySelector("#crossImage");
+// K.A.R.P v0.2 array build unstable
+const array_objects =[
+  { name: "The New Balance C-ms574", description: "Salehe Bembury Shark Skin", image:"sneaker-big__img_NB_C_MS_574.png" },
+  { name: "The New Balance C-ms574", description: "Salehe Bembury Shark Skin", image:"sneaker-big__img_NB_C_MS_574.png" },
+  { name: "The New Balance C-ms574", description: "Salehe Bembury Shark Skin", image:"sneaker-big__img_NB_C_MS_574.png" },
+];
 
-  if (crossName.value && crossDescription.value) {
+const sneaker_container = document.querySelector("#sneakers")
+
+function addCards(array){
+  for (let i = 0; i<array.length;i+=1){
     let newElement = document.createElement("div");
-
-    newElement.innerHTML = `<div class="sneaker sneaker_animation">
-    <img src="${crossImage.value}" class="sneaker__image" id="sneaker-image" alt="${crossName.value + crossDescription.value}">
-      <div class="sneaker__info" id="sneaker-info">
-            <div class="sneaker__title">${crossName.value}</div>
-            <div class="sneaker__description">${crossDescription.value}</div>
-        </div>
-        </div>`;
+    newElement.innerHTML=
+    `
+    <a href="Cross_n_1.html">
+      <div class="sneaker sneaker_animation">
+          <img src="${array[i].image}" class="sneaker__image" id="sneaker-image" alt="none">
+          <div class="sneaker__info">
+              <div class="sneaker__title">
+                  ${array[i].name}
+              </div>
+              <div class="sneaker__description">
+                  ${array[i].description}
+              </div>
+          </div>
+      </div>
+    </a>
+    `;
     sneakers.appendChild(newElement);
     crossName.value = "";
     crossDescription.value = "";
@@ -21,5 +33,18 @@ function addCard() {
   }
 }
 
-const button = document.querySelector("#addButton");
-button.addEventListener("click", addCard);
+function appendToArray(array){
+  const crossName = document.querySelector("#crossName");
+  const crossDescription = document.querySelector("#crossDescription");
+  const crossImage = document.querySelector("#crossImage");
+
+  let array_objects_additive = [crossName.value,crossDescription.value,crossImage.value ]
+
+  array.push(array_objects_additive)
+
+  addCards(array_objects)
+}
+
+const buttonAdd = document.getElementById("addButton");
+buttonAdd.addEventListener("click", appendToArray(array_objects));//append values into main array
+buttonAdd.addEventListener("click",alert("test broadcast message"))//why it displays only on startup and ignores pressing?
